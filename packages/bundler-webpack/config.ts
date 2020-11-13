7974684743d89a2b34d2d32d2f6ceecdb8678ace
srgooglo <chippotato39@gmail.com>
2020-11-13 14:05:08 +0100

    init

---
+++ packages/bundler-webpack/config.ts
@@ -0,0 +1,17 @@
+import nodeExternals from 'webpack-node-externals'
+
+export default {
+  outputPath: './bundled/',
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: {
+    webpackHotDevClient: './src/webpackHotDevClient/webpackHotDevClient',
+  },
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  devtool: false,
+  define: {
+    'process.env': {},
+  },
+};
