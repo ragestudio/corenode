170188f8aac0c2edb45691d4febaa675e4d85744
srgooglo <chippotato39@gmail.com>
2020-12-15 15:17:45 +0100

    remove types

--- packages/nodecorejs/src/cjs.d.ts
+++
@@ -1,8 +0,0 @@
-import * as utils from '@nodecorejs/libs';
-import { Service } from './ServiceWithBuiltIn';
-import { defineConfig } from './defineConfig';
-export * from '@nodecorejs/types';
-export * from '@nodecorejs/runtime';
-export { utils };
-export { Service };
-export { defineConfig };
--- packages/nodecorejs/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Link, NavLink, Prompt, Redirect, Route, Router, StaticRouter, MemoryRouter, Switch, matchPath, withRouter, useHistory, useLocation, useParams, useRouteMatch, __RouterContext, createBrowserHistory, createHashHistory, createMemoryHistory, Plugin, ApplyPluginsType, dynamic, } from '@nodecorejs/runtime';
-export type { MemoryHistory } from '@nodecorejs/runtime';
-export * from '@@/core/coreExports';
