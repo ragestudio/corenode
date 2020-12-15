fedcfe28a157a8973fe8f186abde2f9fd34559f0
srgooglo <chippotato39@gmail.com>
2020-12-15 14:18:28 +0100

    fix packages

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -4,5 +4,5 @@ let command = null;
 try {
     command = require(`${process.cwd()}/node_modules/@nodecorejs/cli/dist/index.js`);
 } catch (e) {
-    command = require(`${__dirname}/dist/index.js`);
+    command = require(`../dist/index.js`);
 }
