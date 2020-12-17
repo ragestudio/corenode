b94acadd0522523e5c545038fae422a1eb423f45
srgooglo <chippotato39@gmail.com>
2020-12-17 12:31:15 +0100

    moved method releaseProyect to index

--- packages/dot-runtime/src/index.ts
+++ packages/dot-runtime/src/index.ts
@@ -6,11 +6,20 @@ import getPackages from './utils/getPackages'
 import fs from 'fs'
 import bootstrap from './bootstrap'
 import { IRuntimeEnv, IPackageJSON } from './types'
-import { release } from './release'
+import chalk from 'chalk'
+import execa from 'execa'
 
 const rootPackageJSON = path.resolve(process.cwd(), './package.json')
 const versionFile = path.resolve(process.cwd(), './.version')
 const findenvs = require('find-up').sync(syncEnvs)
+
+const { join } = require('path')
+const { writeFileSync } = require('fs')
+const newGithubReleaseUrl = require('new-github-release-url')
+const open = require('open')
+const exec = require('./utils/exec')
+const isNextVersion = require('./utils/isNextVersion')
+const { getChangelog } = require('./utils/changelog')
 
 export let version: string
 export let parsedVersion: any = {
@@ -177,11 +186,148 @@ export function syncPackagesVersions() {
     })
 }
 
-export const releaseProyect = () => {
-    release().catch((err) => {
-        console.error(err);
-        process.exit(1);
+function printErrorAndExit(message) {
+  console.error(chalk.red(message));
+  process.exit(1);
+}
+
+function logStep(name) {
+  // TODO: Replace with verbosity API
+  console.log(`${chalk.gray('>> Release:')} ${chalk.magenta.bold(name)}`);
+}
+
+export const releaseProyect = async (args:any) => {
+    let opts = { 
+        skipGitStatusCheck: false,
+        publishOnly: false,
+        skipBuild: false
+    }
+  
+    if (typeof(args) !== "undefined") {
+        opts = { ...opts, ...args }
+    }
+
+    // Check git status
+    if (!opts.skipGitStatusCheck) {
+        const gitStatus = execa.sync('git', ['status', '--porcelain']).stdout;
+        if (gitStatus.length) {
+            printErrorAndExit(`Your git status is not clean. Aborting.`);
+        }
+    } else {
+        logStep(
+            'git status check is skipped, since --skip-git-status-check is supplied',
+        );
+    }
+
+    // get release notes
+    logStep('get release notes');
+    const releaseNotes = await getChangelog();
+    console.log(releaseNotes(''));
+
+    // Check npm registry
+    logStep('check npm registry');
+    const userRegistry = execa.sync(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['config', 'get', 'registry']).stdout;
+    if (userRegistry.includes('https://registry.yarnpkg.com/')) {
+        printErrorAndExit(
+            `Release failed, please use ${chalk.blue('npm run release')}.`,
+        );
+    }
+    if (!userRegistry.includes('https://registry.npmjs.org/')) {
+        const registry = chalk.blue('https://registry.npmjs.org/');
+        printErrorAndExit(`Release failed, npm registry must be ${registry}.`);
+    }
+
+    if (!opts.publishOnly) {
+        // Build
+        if (!opts.skipBuild) {
+            logStep('build');
+            await exec('nodecore', ['build']);
+        } else {
+            logStep('build is skipped, since args.skipBuild is supplied');
+        }
+
+        // Bump version
+        bumpVersion(["minor"])
+
+        // Sync version to root package.json
+        logStep('sync version to root package.json');
+        syncPackagesVersions()
+        const rootPkg = require('../package');
+        Object.keys(rootPkg.devDependencies).forEach((name) => {
+            if (name.startsWith('@nodecorejs/')) {
+                rootPkg.devDependencies[name] = version;
+            }
+        });
+        writeFileSync(join(process.cwd(), '..', 'package.json'), JSON.stringify(rootPkg, null, 2) + '\n', 'utf-8');
+
+        // Commit
+        const commitMessage = `release: v${version}`;
+        logStep(`git commit with ${chalk.blue(commitMessage)}`);
+        await exec('git', ['commit', '--all', '--message', commitMessage]);
+
+        // Git Tag
+        logStep(`git tag v${version}`);
+        await exec('git', ['tag', `v${version}`]);
+
+        // Push
+        logStep(`git push`);
+        await exec('git', ['push', 'origin', 'master', '--tags']);
+    }
+
+    const currVersion = getVersion()
+    // Publish
+    if (!runtimeEnv.devRuntime) {
+        return printErrorAndExit(`headPackage is missing on runtime`);
+    }
+
+    const pkgs = getPackages();
+    logStep(`publish packages: ${chalk.blue(pkgs.join(', '))}`);
+    const isNext = isNextVersion(currVersion);
+    pkgs.sort((a) => {
+        return a === runtimeEnv.devRuntime.headPackage ? 1 : -1;
+    })
+        .forEach((pkg, index) => {
+            const pkgPath = join(process.cwd(), 'packages', pkg);
+            const { name, version } = require(join(pkgPath, 'package.json'));
+            if (version === currVersion) {
+                console.log(
+                    `[${index + 1}/${pkgs.length}] Publish package ${name} ${isNext ? 'with next tag' : ''
+                    }`,
+                );
+                const cliArgs = isNext ? ['publish', '--tag', 'next'] : ['publish'];
+                try {
+                    const { stdout } = execa.sync('npm', cliArgs, {
+                        cwd: pkgPath,
+                    })
+                    console.log(stdout);
+                } catch (error) {
+                    console.log(`❌ Failed to publish > ${pkg} >`, err)
+                }
+            }
+        });
+
+
+    if (!runtimeEnv.devRuntime.originGit) {
+        return printErrorAndExit(`originGit is missing on runtime`);
+    }
+
+    logStep('create github release');
+    const tag = `v${currVersion}`;
+    const changelog = releaseNotes(tag);
+    console.log(changelog);
+    const url = newGithubReleaseUrl({
+        repoUrl: runtimeEnv.devRuntime.originGit,
+        tag,
+        body: changelog,
+        isPrerelease: isNext,
     });
+    try {
+        await open(url);
+    } catch (error) {
+        console.log("Try opening url >", url)
+    }
+
+    logStep('done');
 }
 
 export default runtimeEnv
