7628d90d42fde8ef7a52d4b9cfec2a274be22fe8
srgooglo <chippotato39@gmail.com>
2020-12-21 07:35:48 +0100

    removed cli script command

--- packages/nodecorejs/cli/nodecore.js
+++ packages/nodecorejs/cli/nodecore.js
@@ -3,19 +3,18 @@ const fs = require("fs")
 const path = require("path")
 const process = require("process")
 
-let command = null;
 const isDev = fs.existsSync(path.resolve(__dirname, '../.local'))
 
 try {
     console.log(`${isDev? 'Running NodecoreJS on .local mode' : ''}`)
     if (isDev){
-        command = require(`${process.cwd()}/packages/cli/dist`);
+        require(`${process.cwd()}/packages/cli/dist`);
     }else {
-        command = require(`${process.cwd()}/node_modules/@nodecorejs/cli/dist`);
+        require(`${process.cwd()}/node_modules/@nodecorejs/cli/dist`);
     }
 } catch (e) {
     try {
-        command = require(`${process.cwd()}/node_modules/@nodecorejs/cli/dist`);
+        require(`${process.cwd()}/node_modules/@nodecorejs/cli/dist`);
     } catch (error) {
         console.log(`❌ Nodecore failed to load CLI`)
         console.log(e)
