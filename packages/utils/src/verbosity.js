5367723305f3284c7ceedb85bbf15368b7adb3de
srgooglo <chippotato39@gmail.com>
2021-01-08 11:19:18 +0100

    ✳️ Rewrited methods & new class & improve quality and reliability

--- packages/utils/src/verbosity.js
+++ packages/utils/src/verbosity.js
@@ -4,136 +4,187 @@ import chalk from 'chalk'
 import { objectToArrayMap } from './objectToArray'
 import chalkRandomColor from './chalkRandomColor'
 import log4js from 'log4js'
+import { isNumber } from 'lodash'
 
-const verbosify = (data, opts, colorsOpts) => {
-    let { log, trace } = data
+const stack = stackTrace.get()[1]
 
-    let decoratorStr = ''
-    let logStr = []
+const getStack = {
+    line: `(:${stack.getLineNumber()})`,
+    file: path.basename(stack.getFileName()),
+    method: `[${stack.getFunctionName()}]`
+}
 
-    let decoratorData = {
-        time: new Date().toLocaleTimeString(),
-        line: null,
-        file: null,
-        method: null
+const defaultColors = Object.freeze({
+    decorator: {
+        text: "blue",
+        bg: false
+    },
+    log: {
+        text: false,
+        bg: false
+    }
+})
+const defaultDecoratorOptions = Object.freeze({
+    prefix: false,
+    time: true,
+    line: false,
+    file: false,
+    method: true,
+})
+const defaultDecoratorData = Object.freeze({
+    prefix: null,
+    time: new Date().toLocaleTimeString(),
+    line: getStack.line,
+    file: getStack.file,
+    method: getStack.method
+})
+
+class verbosify {
+    constructor(data, options, colors) {
+        this.options = { ...defaultDecoratorOptions }
+
+        this.colors = { ...defaultColors }
+        this.decoratorData = { ...defaultDecoratorData }
+
+        try {
+            let generationInstance = []
+
+            if (!data) {
+                return null
+            }
+
+            if (colors) {
+                this.colors = { ...this.colors, ...colors }
+            }
+            if (options) {
+                this.options = { ...this.options, ...options }
+            }
+
+            objectToArrayMap(this.options).forEach((element) => {
+                let fromData = this.decoratorData[element.key]
+
+                const isElementString = typeof (element.value) === "string"
+                const isElementNumber = typeof (element.value) === "number"
+
+                if (isElementString || isElementNumber) {
+                    fromData = String(element.value)
+                }
+
+                const isValidOption = element.value != null && element.value || isElementNumber
+                const isValidData = typeof (fromData) !== "undefined" && fromData || typeof (fromData) === "number"
+
+                if (isValidOption && isValidData) {
+                    generationInstance.push(fromData)
+                }
+            })
+
+            const decoratorString = this.generateDecorator(generationInstance, this.colors)
+            const logString = this.generateLogString(data.log, this.colors)
+
+            return [decoratorString, ...logString]
+        } catch (error) {
+            // woupsi
+            console.log(error)
+        }
     }
 
-    try {
-        if (opts) {
-            if (typeof (trace) !== "undefined" && trace != null) {
-                objectToArrayMap(decoratorData).forEach((element) => {
-                    if (typeof (opts[element.key]) == "string") {
-                        decoratorData[element.key] = opts[element.key]
-                    }
-                    if (opts[element.key] === true && trace[element.key]) {
-                        decoratorData[element.key] = trace[element.key]
-                    }
-                })
-            }
+    generateDecorator = (input, colors) => {
+        let output = ''
+        for (let index = 0; index < input.length; index++) {
+            const element = input[index]
+            const divisor = (index == (input.length - 1) ? " > " : " | ")
+
+            output = `${output}${element}${divisor}`
         }
+        return this.paint('decorator', output, colors)
+    }
 
-        const paint = (type, data) => {
-            if (!colorsOpts) {
-                return data
-            }
-            if (typeof (data) !== "string") {
-                return data
-            }
-            let target = chalk
-
-            const colors = colorsOpts
-
-            let textColor = colors[type].text
-            let backgroundColor = colors[type].bg
-
-            if (textColor) {
-                if (target[textColor]) {
-                    target = target[textColor]
-                }
-            }
-
-            if (backgroundColor) {
-                if (target[backgroundColor]) {
-                    target = target[backgroundColor]
-                }
-            }
-
-            return target(data)
-        }
-
-        const decoratorMap = objectToArrayMap(decoratorData).filter(element => element.value != null)
-        for (let index = 0; index < decoratorMap.length; index++) {
-            const element = decoratorMap[index]
-            const divisor = (index == (decoratorMap.length - 1) ? " > " : " | ")
-            if (element.value != null && typeof (element.value) !== "undefined") {
-                decoratorStr = `${decoratorStr}${element.value}${divisor}`
-            }
-        }
-        decoratorStr = paint('decorator', decoratorStr)
-
-        const logMap = objectToArrayMap(log)
+    generateLogString = (input, colors) => {
+        let output = []
+        const logMap = objectToArrayMap(input)
         for (let index = 0; index < logMap.length; index++) {
-            const element = logMap[index];
-            logStr.push(paint('log', element.value))
+            const element = logMap[index]
+            output.push(this.paint('log', element.value, colors))
+        }
+        return output
+    }
+
+    paint = (type, data, colors) => {
+        if (!colors) {
+            return data
+        }
+        if (typeof (data) !== "string") {
+            return data
+        }
+        let target = chalk
+
+        const colorsOpt = colors
+
+        let textColor = colorsOpt[type].text
+        let backgroundColor = colorsOpt[type].bg
+
+        if (textColor) {
+            if (target[textColor]) {
+                target = target[textColor]
+            }
         }
 
-        return [decoratorStr, ...logStr]
-    } catch (error) {
-        return error
+        if (backgroundColor) {
+            if (target[backgroundColor]) {
+                target = target[backgroundColor]
+            }
+        }
+
+        return target(data)
     }
 }
 
-const trace = stackTrace.get()[1]
-const line = `(:${trace.getLineNumber()})`
-const file = path.basename(trace.getFileName())
-const method = `[${trace.getFunctionName()}]`
-
 export default {
-    opts: {
-        line: false,
-        method: true,
-        file: false,
-        time: true,
-    },
-    colorsOpts: {
-        decorator: {
-            text: "blue",
-            bg: false
-        },
-        log: {
-            text: false,
-            bg: false
-        }
-    },
-    vrb: function (opts, colors, ...context) {
-        return verbosify({
-            trace: {
-                file,
-                line,
-                method,
-            },
-            log: {
-                ...context
-            }
-        }, opts, colors)
+    _options: {},
+    _colors: {},
+    output: function (t, o, c, ...context) {
+        let response = new verbosify({ log: { ...context } }, o, c)
+        console[t](...response)
+        // force to flush
+        this._options = {}
+        this._colors = {}
+
+        return this
     },
     log: function (...context) {
-        let response = this.vrb(this.opts, this.colorsOpts, ...context)
-        console.log(...response)
+        try {
+            this.output('log', this._options, this._colors, ...context)
+        } catch (error) {
+            // woupssi
+        }
         return this
     },
     error: function (...context) {
-        let response = this.vrb(this.opts, {
-            decorator: {
-                text: "orange",
-                bg: false
-            },
-            log: {
-                text: "red",
-                bg: false
-            }
-        }, ...context)
-        console.error(...response)
+        try {
+            this.output('error', {
+                ...this._options,
+                prefix: "❌ ERROR"
+            }, {
+                decorator: {
+                    text: "black",
+                    bg: "bgYellow"
+                },
+                log: {
+                    text: "red",
+                    bg: false
+                }
+            }, ...context)
+        } catch (error) {
+            // woupssi
+        }
+        return this
+    },
+    random: function (...context) {
+        try {
+            this.output('log', this._options, null, chalkRandomColor(...context))
+        } catch (error) {
+            // woupssi
+        }
         return this
     },
     dump: function (...context) {
@@ -148,17 +199,17 @@ export default {
         const logger = log4js.getLogger("logs")
 
         logger.debug(...context)
-        return this
-    },
-    random: function (...context) {
-        let response = this.vrb(this.opts, null, ...context)
-        console.log(chalkRandomColor(response))
+
         return this
     },
     options: function (params) {
         // TODO: Options validation
-        if (typeof (params) !== "undefined" && params != null) {
-            this.opts = { ...this.opts, ...params }
+        try {
+            if (typeof (params) !== "undefined" && params != null) {
+                this._options = { ...this._options, ...params }
+            }
+        } catch (error) {
+            // terrible
         }
         return this
     },
@@ -166,8 +217,8 @@ export default {
         if (typeof (params) !== "undefined" && params != null) {
             try {
                 objectToArrayMap(params).forEach((param) => {
-                    if (this.colorsOpts[param.key]) {
-                        this.colorsOpts[param.key] = { ...this.colorsOpts[param.key], ...param.value }
+                    if (this._colors[param.key]) {
+                        this._colors[param.key] = { ...this._colors[param.key], ...param.value }
                     }
                 })
             } catch (error) {
