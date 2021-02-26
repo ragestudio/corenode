3528b4f11d92df40ce0d1c39d570b3e5e9cb2e89
srgooglo <chippotato39@gmail.com>
2021-02-26 16:59:58 +0100

    replaced localBin method

--- packages/nodecorejs/cli/nodecore.js
+++ packages/nodecorejs/cli/nodecore.js
@@ -5,10 +5,22 @@ const process = require("process")
 
 const prodBin = path.resolve(__dirname, '../node_modules/@nodecorejs/cli/dist')
 const localBin = `${process.cwd()}/packages/cli/dist`
+const localPkgJson = `${process.cwd()}/package.json`
 
+let isLocalMode = false
 let targetBin = null
 
-process.env.LOCAL_BIN = false
+if (fs.existsSync(localPkgJson)) {
+    try {
+        const pkg = require(localPkgJson)
+        if (pkg.name === "nodecorejs") {
+            isLocalMode = true
+        }
+    } catch (error) {
+        console.log(`❌ Error processing package.json > ${error.message}`)
+    }
+}
+
 if (isLocalMode) {
     targetBin = localBin
 } else {
