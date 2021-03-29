76120203d917673e7c14403cd0d2d0df3f8d90a5
srgooglo <traveld8@gmail.com>
2021-03-29 20:11:23 +0200

    build builder from postinstall script

--- postinstall/index.js
+++ postinstall/index.js
@@ -1,6 +1,20 @@
 // TODO: Complete postinstall development script
 
-// - [] Build entire proyect
+// cd ./packages/builder && npm i && yarn build && cd ../../ && yarn run builder
+// - [] Run install dependencies
+// - [x] Build entire proyect
 // - [] Link packages as dependencies
 // - [] Link internals modules to /nodecorejs/internals 
 
+const path = require("path")
+const fs = require("fs")
+const execa = require("execa")
+
+const builderSrcPath = path.resolve(process.cwd(), `packages/builder/src`)
+const builderDistPath = path.resolve(process.cwd(), `packages/builder/dist`)
+
+if (!fs.existsSync(builderSrcPath)) {
+    throw new Error(`Builder not exists`)
+}
+
+execa.sync('babel', [`${builderSrcPath}`, `--out-dir`, `${builderDistPath}`])
