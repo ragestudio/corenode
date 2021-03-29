b1fcd7990ce20635fd4ebebfcc9a5ac6645444e0
srgooglo <chippotato39@gmail.com>
2021-03-29 15:39:20 +0200

    added utest

---
+++ utests/modules.js
@@ -0,0 +1,6 @@
+const modules = require('../packages/nodecorejs/dist/modules/index2.js').default
+
+
+const modulesController = new modules()
+
+modulesController.fetchInternals()
---
+++ utests/objectToArray.js
@@ -0,0 +1,9 @@
+const { objectToArrayMap } = require('@nodecorejs/utils/dist/objectToArray')
+
+const testMap = objectToArrayMap({ bruh: "nose", otra: "cosa" })
+
+console.log(testMap)
+
+testMap.forEach((entry) => {
+    console.log(entry)
+})
