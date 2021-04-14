31f640a5ea6a33931c61b879d0514e7f5b6a8767
srgooglo <chippotato39@gmail.com>
2021-04-14 18:08:40 +0200

    added yarg parser

--- packages/builtin-lib/src/cli/index.js
+++ packages/builtin-lib/src/cli/index.js
@@ -1,5 +1,8 @@
+import yparser from 'yargs-parser'
+
 export default {
     argv: process.argv,
+    args: yparser(process.argv.slice(2)),
     add: (command) => {
         if (typeof (command) == "object") {
             if (typeof (global.corenode_cli.custom) == "undefined") {
