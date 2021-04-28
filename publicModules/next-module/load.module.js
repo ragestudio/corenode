f3bb4f495ce321421ad8a31d25eb98c85953793a
srgooglo <chippotato39@gmail.com>
2021-04-28 16:49:03 +0200

    added lib

--- publicModules/next-module/load.module.js
+++ publicModules/next-module/load.module.js
@@ -1,13 +1,14 @@
-const moduleFile = require("../dist/index.js")
-
 module.exports = {
     pkg: "next-module",
     init: (lib) => {
         const { cli } = lib.builtIn
+        const moduleLib = require("./dist/index.js")
+
         cli.add({
             command: "next-dev",
             exec: () => {
                 console.log("Starting Next.JS development server...")
+                moduleLib.init()
             }
         })
     }
--- publicModules/next-module/src/index.js
+++ publicModules/next-module/src/index.js
@@ -1,3 +1,32 @@
-import corenode from 'corenode'
+const { verbosity } = require('@corenode/utils')
+const express = require('express')
+const next = require('next')
+const nextI18NextMiddleware = require('next-i18next/middleware').default
 
-console.log(corenode)
+const nextI18next = require('./i18n')
+
+const moduleEnv = global._env.nextModule 
+
+const port = moduleEnv.port ?? 8050;
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+
+export async function initApp() {
+    await app.prepare()
+    const server = express()
+
+    server.use(nextI18NextMiddleware(nextI18next))
+
+    server.get(/\.css$/, (req, res, nextHandler) => {
+        res.setHeader(
+            "Cache-Control",
+            "public, max-age=0, immutable",
+        );
+        nextHandler()
+    })
+
+    server.get('*', (req, res) => handle(req, res))
+
+    await server.listen(port)
+    verbosity.log(`> Ready on http://localhost:${port}`)
+}
