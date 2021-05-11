8f3e9b4dfcd1f4f1b55002c52deefebe5fad99e7
srgooglo <chippotato39@gmail.com>
2021-05-11 20:05:58 +0200

    😳

--- packages/corenode/src/vm/objects/index.js
+++ packages/corenode/src/vm/objects/index.js
@@ -6,6 +6,45 @@ let vmObjects = {
             super(...context)
             verbosity.options({ method: `[Unhandled ThrowError]`, dump: true, dumpFile: true }).error(...context)
         }
+    },
+    Cum: function (text) {
+        const whiteBGScape = `\x1b[47m`
+        const whiteFGScape = `\x1b[37m`
+
+        const blackFGScape = `\x1b[30m`
+        const resetScape = `\x1b[0m`
+
+        const { columns, rows } = process.stdout
+
+        let emptyLine = String()
+        let centerLine = String()
+
+        for (let index = 0; index < columns; index++) {
+            if (index == (rows / 2)) {
+                centerLine = `${centerLine}${text}`
+            } else {
+                centerLine = `${centerLine}-`
+            }
+        }
+
+        for (let index = 0; index < columns; index++) {
+            emptyLine = `${emptyLine}-`
+        }
+
+        for (let index = 0; index < rows; index++) {
+            if (index == 0) {
+                console.log(`${whiteBGScape}${whiteFGScape}`)
+            }
+
+            if (index == (rows / 2)) {
+                
+                console.log(`${blackFGScape}${centerLine}${whiteFGScape}`)
+            } else {
+                console.log(`${emptyLine}`)
+            }
+        }
+
+        console.log(resetScape)
     }
 }
 
