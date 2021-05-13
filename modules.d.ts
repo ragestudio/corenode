3c044af92a9c26bc861824a689a416f259ff71bd
srgooglo <chippotato39@gmail.com>
2021-05-13 11:05:12 +0200

    removed env interface modules.d.ts

--- modules.d.ts
+++ modules.d.ts
@@ -1,20 +1,3 @@
-declare namespace corenodeJS {
-  export interface ProcessEnv {
-    corenode_ENV: string;
-    corenode_VERSION: string;
-    NODE_ENV: 'production' | 'development' | 'test';
-    HTTPS: string;
-    HOST: string;
-    PORT: string;
-    APP_ROOT: string;
-    CLEAR_OUTPUT: null | 'none';
-    RM_TMPDIR: null | 'none';
-    ERROR_CODE_MAP_PATH: string;
-    SPEED_MEASURE: null | 'CONSOLE';
-    BABEL_CACHE: null | 'none';
-    COMPRESS: null | 'none';
-    WATCH: null | 'none';
-    WATCH_IGNORED: null | string;
-    SYSTEM_BELL: null | 'none';
-  }
+declare namespace corenode {
+
 }
