f31c3a97f3bb9d6aaadae398356b7929c8adfbed
srgooglo <chippotato39@gmail.com>
2021-06-03 15:58:23 +0200

    added ual basics

---
+++ packages/ual/src/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const defaultOrigin = "https://registry.ragestudio.net"
+
+const axiosInstance = axios.create({
+    baseURL: defaultOrigin
+})
+
+function getAuth() {
+    
+}
+
+function remove() {
+
+}
+
+function add() {
+
+}
+
+function checkValidity() {
+
+}
+
+function isUpToDate() {
+
+}
+
+function fetchManifest(id) {
+
+}
