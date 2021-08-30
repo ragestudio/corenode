db6bcd009032a220d5e33752ab74f0ac2db8a7da
srgooglo <srgooglo@ragestudio.net>
2021-08-30 12:48:13 +0200

    fix dev-addon

--- addons/dev-addon/load.addon.js
+++ addons/dev-addon/load.addon.js
@@ -1,20 +1,4 @@
 module.exports = {
     pkg: "dev",
     script: "./addon/index.js",
-    ignoreDependencies: true,
-    appendCli: [
-        {
-            command: "dev [file]",
-            exec: (context, args) => {
-                const script = context.machine.dispatcher()
-
-                if (!args.file) {
-                    // exit
-                    return false
-                }
-
-                script.watch({ file: args.file })
-            }
-        }
-    ],
 }
