3256927e3894b61f4dec83f68853750134efb362
srgooglo <srgooglo@ragestudio.net>
2021-09-15 15:08:19 +0200

    update events

--- src/events.js
+++ src/events.js
@@ -1,9 +1,31 @@
+const constables = require('./constables')
+const open = require('open')
+
 module.exports = [
     {
-        on: "cli_noCommand",
+        on: "fatalCrash",
         event: () => {
-            const repl = require('./repl')
-            repl.attachREPL()
+            const file = constables.fatalCrashLogFile
+            const now = new Date()
+            const err = `
+            --------------------
+            \n
+            🆘 >> [${now.toLocaleDateString()} ${now.toLocaleTimeString()}]
+            \n\t ${error.stack}
+            \n
+            --------------------\n
+            `
+        
+            fs.appendFileSync(file, err, { encoding: "utf-8" })
+        
+            console.log(`[🛑 FATAL CRASH] > ${error.message}`)
+            console.log(`🗒  Open '${file}' for more details >> ${file}`)
+        
+            try {
+                open(file)
+            } catch (error) {
+                // fatality, something is really broken ._.
+            }
         } 
     }
 ]
