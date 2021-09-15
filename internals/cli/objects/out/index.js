d4e50d7bf1040e41e7739051c116accf37882924
srgooglo <srgooglo@ragestudio.net>
2021-09-15 15:07:16 +0200

    removed duplicated dir

--- internals/cli/objects/out/index.js
+++
@@ -1,6 +0,0 @@
-const { verbosity } = require('@corenode/utils')
-
-module.exports = function (...args) {
-    const v = verbosity.options({ method: `[${this.id ? this.id : "out"}]` })
-    v.log(...args)
-}
