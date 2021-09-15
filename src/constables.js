cec1b95beabfb8a9d5c6c077bbaa4dd1a23441f5
srgooglo <srgooglo@ragestudio.net>
2021-09-15 15:08:41 +0200

    added `fatalCrashLogFile` into constables

--- src/constables.js
+++ src/constables.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 module.exports = {
     // strings
     INVALID_LOCALMODE_FLAG: `⚠️  'LOCAL_BIN' environment flag is enabled, but this project is not allowed to run in local mode, ignoring running in local mode!`,
@@ -6,5 +8,5 @@ module.exports = {
     ERROR_PROCESSING_PACKAGE: `❌ Error processing package.json`,
     
     // vars
-
+    fatalCrashLogFile: process.env.fatalCrashLogFile ?? path.resolve(process.cwd(), '.crash.log')
 }
