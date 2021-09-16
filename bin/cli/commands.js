ebb5aab8ef4f8b9ea6aa606a3a71a3f0dec656a7
srgooglo <srgooglo@ragestudio.net>
2021-09-16 12:12:13 +0200

    added bump-version command

--- bin/cli/commands.js
+++ bin/cli/commands.js
@@ -50,6 +50,25 @@ module.exports = [
         }
     },
     {
+        command: "bump-version",
+        description: "Bump version of the project",
+        arguments: ["<type...>"],
+        exec: (type) => {
+            const bumps = []
+            
+            type.forEach((bump) => {
+                if (!bumps.includes(bump)) {
+                    bumps.push(bump)
+                }
+            })
+
+            if (bump.length > 0) {
+                process.runtime.helpers.bumpVersion(bumps)
+                process.runtime.helpers.syncVersions()
+            }
+        },
+    },
+    {
         command: 'version',
         description: "Manage project version",
         exec: (argv) => {
