0477a02429899fc42894309684eae660175390fc
srgooglo <srgooglo@ragestudio.net>
2021-09-17 11:57:20 +0200

    changed target

--- src/bin/cli/index.js
+++ src/bin/cli/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const { runInNewRuntime } = require('corenode')
+const { runInNewRuntime } = require('../../index.js')
 
 runInNewRuntime((runtime) => {
     const { program, Command, Option } = require('commander')
