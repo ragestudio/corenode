467af9572385fd280458c48c675dcc484dbbe112
srgooglo <srgooglo@ragestudio.net>
2021-09-23 09:30:14 +0200

    split to functions

--- src/vm/serialize.js
+++ src/vm/serialize.js
@@ -1,61 +1,59 @@
-export class Serializer {
-    deserialize(input) {
-        const datatype = typeof input
+function deserialize(input) {
+    const datatype = typeof input
 
-        switch (datatype) {
-            case "object": {
-                let tmp = {}
+    switch (datatype) {
+        case "object": {
+            let tmp = {}
 
-                const objectKeys = Object.keys(input)
+            const objectKeys = Object.keys(input)
 
-                objectKeys.forEach((key) => {
-                    tmp[key] = this.deserialize(input[key])
-                })
+            objectKeys.forEach((key) => {
+                tmp[key] = this.deserialize(input[key])
+            })
 
-                input = tmp
-                break
-            }
-            case "function": {
-                input = new Function(input)
-                break
-            }
-            default:
-                break
+            input = tmp
+            break
         }
-
-        if (Object.keys(input).length <= 0) {
-            return false
+        case "function": {
+            input = new Function(input)
+            break
         }
-
-        return input
+        default:
+            break
     }
 
-    serialize(input) {
-        const datatype = typeof input
-
-        switch (datatype) {
-            case "object": {
-                let tmp = {}
-
-                const objectKeys = Object.keys(input)
-
-                objectKeys.forEach((key) => {
-                    tmp[key] = this.serialize(input[key])
-                })
-
-                input = tmp
-                break
-            }
-            case "function": {
-                input = input.toString()
-                break
-            }
-            default:
-                break
-        }
-
-        return input
+    if (Object.keys(input).length <= 0) {
+        return false
     }
+
+    return input
 }
 
-export default new Serializer()
+function serialize(input) {
+    const datatype = typeof input
+
+    switch (datatype) {
+        case "object": {
+            let tmp = {}
+
+            const objectKeys = Object.keys(input)
+
+            objectKeys.forEach((key) => {
+                tmp[key] = this.serialize(input[key])
+            })
+
+            input = tmp
+            break
+        }
+        case "function": {
+            input = input.toString()
+            break
+        }
+        default:
+            break
+    }
+
+    return input
+}
+
+module.exports = { serialize, deserialize }
