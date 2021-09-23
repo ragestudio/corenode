f2de9d27a53af74049f8a393ab39b00109a8b43e
srgooglo <srgooglo@ragestudio.net>
2021-09-23 09:30:04 +0200

    removed unused methods

--- src/logger/index.js
+++ src/logger/index.js
@@ -1,6 +1,5 @@
 import { createLogger, format, transports } from 'winston'
 import StackTrace from 'stacktrace-js'
-import { verbosity } from '@corenode/utils'
 
 const { combine, timestamp, label, printf } = format
 class Logger {
@@ -8,38 +7,11 @@ class Logger {
         this.id = params.id ?? null
     }
 
-    log = (...context) => {
-        StackTrace.get().then((stack) => {
-            stack = stack[2]
-            this.output({ level: "log", stack }, ...context)
-        })
-    }
-
-    warn = (...context) => {
-        StackTrace.get().then((stack) => {
-            stack = stack[2]
-            this.output({ level: "warn", stack }, ...context)
-        })
-    }
-    error = (...context) => {
-        StackTrace.get().then((stack) => {
-            stack = stack[2]
-            this.output({ level: "error", stack }, ...context)
-        })
-    }
-
     dump = (level, data) => {
         StackTrace.get().then((stack) => {
             stack = stack[2]
             this.createDump({ level: level, stack }).info(data)
         })
-    }
-
-    output(payload, ...context) {
-        const { level, stack } = payload
-
-        this.createDump({ level, stack }).info(...context)
-        verbosity.options({ method: `[${this.id? `${this.id} | ` : ""}${stack.functionName}]` })[level](...context)
     }
 
     createDump({ level, stack }) {
