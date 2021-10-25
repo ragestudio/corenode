0a1255ca9238426f80a0c1a51d8be1d6037026a1
srgooglo <srgooglo@ragestudio.net>
2021-10-25 11:17:26 +0200

    on exec require

--- src/bin/cli/commands/changelogs/index.js
+++ src/bin/cli/commands/changelogs/index.js
@@ -1,11 +1,10 @@
-const { getChangelogs } = require('@corenode/git-lib')
-
 module.exports = {
     command: 'changelogs',
     arguments: ["[to]", "[from]"],
     description: "Show the changelogs of this project from last tag",
     exec: async (to, from) => {
-        const changes = await getChangelogs(process.runtime.helpers.getOriginGit(), to, from)
+        const lib = require('@corenode/git-lib')
+        const changes = await lib.getChangelogs(process.runtime.helpers.getOriginGit(), to, from)
         console.log(changes)
     }
 }
